fdc63c00eae359524234c9e07115e29bd0278f95
Husaam NT <husnaeem@gmail.com>
2022-11-19 15:37:57 +0000

    added correct function output syntax

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var runningSum = function(nums) {
        j=nums[i]+j
     }
     console.log(j);
+
 };
-runningSum[1,2,3,4];
+runningSum([1,2,3,4]);
 
 // Input: nums = [1,2,3,4]
 // Output: [1,3,6,10]
