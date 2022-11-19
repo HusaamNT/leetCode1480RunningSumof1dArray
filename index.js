9a9a3b072da21cafe19400f52012e62d4410744b
Husaam NT <husnaeem@gmail.com>
2022-11-19 15:32:42 +0000

    added function code

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 var runningSum = function(nums) {
-    
+    let j=0;
+    const numbers = nums.length;
+    for(i=0; i<numbers; i++){
+       j=nums[i]+j
+    }
+    console.log(j);
 };
+runningSum[1,2,3,4];
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
